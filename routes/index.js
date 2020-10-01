07c8751a143866ae2dac58d1fc571e12e833b333
wrafael91 <wrafaelmp91@gmail.com>
2020-09-30 21:04:50 -0500

    formulario enviando y recibiendo datos

--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,23 @@ const router = express.Router();
 
 const dataInfo = require('../models/schema');
 
-router.get('/', async (req, res) => {
+// Muestra el formulario en pantalla
+router.get('/', (req, res) => {
+    res.render('index');
+});
+// Envia los datos desde el formulario al cuadro de texto
+router.post('/add', async (req, res) => {
+    const saveInfo = new dataInfo(req.body);
+    await saveInfo.save();
+    res.redirect('/datainfo');
+});
+
+// Recibe los datos desde el formulario y los muestra en el cuadro de texto
+router.get('/datainfo', async (req, res) => {
     const saveInfo_ = await dataInfo.find();
     res.render('data', {
         saveInfo_ 
     });
-});
-
-
-router.post('/add', async (req, res) => {
-    const saveInfo = new dataInfo(req.body);
-    await saveInfo.save();
-    res.send('received')
 })
 
 module.exports = router;
